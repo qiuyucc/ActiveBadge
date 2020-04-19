4cf1faca062a876f4f076396a0275f2b9d5de303
qiuyucc <china.cqy@outlook.com>
2020-04-19 15:13:45 +1000

    new changes

--- src/Redux/ConfigureStore.js
+++ src/Redux/ConfigureStore.js
@@ -1,16 +1,24 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import {dishes} from "./dishes";
+import {AsyncStorage} from "react-native";
+import { persistStore, persistReducer} from "redux-persist";
+import reducers from './Reducer';
 
+const persistConfig={
+    key:'root',
+    storage:AsyncStorage,
+    whitelist: ["authReducer", "userReducer"]
+}
 
-export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            dishes
-        }),
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default() => {
+    let store = createStore(
+        persistedReducer,{},
         applyMiddleware(thunk, logger)
     );
+    let persistor = persistStore(store);
 
-    return store;
+    return {store, persistor};
 }
---
+++ src/screens/Main.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    StatusBar ,
+    TouchableOpacity
+} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import {connect} from "react-redux";
+
+import Routes from './Routers';
+
+class Main extends Component<> {
+
+    render() {
+        const {authData:{isLoggedIn}} = this.props;
+        return(
+            <View style={styles.container}>
+                <StatusBar
+                    backgroundColor="#1c313a"
+                    barStyle="light-content"
+                />
+                <Routes isLoggedIn={isLoggedIn} />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1
+    }
+});
+
+const mapStateToProps = state => ({
+    authData: state.authReducer.authData
+})
+
+export default connect(mapStateToProps, null)(Main)
